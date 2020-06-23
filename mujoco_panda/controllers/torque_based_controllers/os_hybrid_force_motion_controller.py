bug fix in force torque meausurements, ee assignment, hybrid control computation

mujoco_panda/controllers/torque_based_controllers/os_hybrid_force_motion_controller.py
@@ -115,8 +115,8 @@ class OSHybridForceMotionController(ControllerBase):
 
         delta_omg = self._pos_o_dir.dot(self._goal_omg - curr_omg)
 
-        # compute force control part along force dimensions
-        force_control = self._force_dir.dot(self._kp_f.dot(delta_force) - np.abs(self._kd_f.dot(delta_vel)) + self._goal_force)
+        # compute force control part along force dimensions # negative sign to convert from experienced to applied
+        force_control = -self._force_dir.dot(self._kp_f.dot(delta_force) - np.abs(self._kd_f.dot(delta_vel)) + self._goal_force)
 
         # compute position control force along position dimensions (orthogonal to force dims)
         position_control = self._pos_p_dir.dot(
@@ -135,8 +135,9 @@ class OSHybridForceMotionController(ControllerBase):
             ori_pos_ctrl = self._pos_o_dir.dot(
                 self._kp_o.dot(delta_ori) + self._kd_o.dot(delta_omg))
 
-            # compute torque control force along torque dimensions (orthogonal to orientation dimensions)
-            torque_f_ctrl = self._torque_dir.dot(self._kp_t.dot(
+            # compute torque control force along torque dimensions (orthogonal to orientation dimensions) 
+            # negative sign to convert from experienced to applied
+            torque_f_ctrl = - self._torque_dir.dot(self._kp_t.dot(
                 delta_torque) - self._kd_t.dot(delta_omg) + self._goal_torque)
 
             # total torque in cartesian at end-effector
@@ -180,9 +181,9 @@ class OSHybridForceMotionController(ControllerBase):
         self._goal_vel = goal_vel
         self._goal_omg = goal_omg
         if goal_force is not None:
-            self._goal_force = goal_force
+            self._goal_force = - np.asarray(goal_force) # applied force = - experienced force
         if goal_torque is not None:
-            self._goal_torque = goal_torque
+            self._goal_torque = - np.asarray(goal_torque) # applied torque = - experienced torque
         self._mutex.release()
 
     def change_ft_dir(self, directions):

mujoco_panda/mujoco_robot.py
@@ -30,6 +30,11 @@ class MujocoRobot(object):
 
     def __init__(self, model_path, render=True, config=None, prestep_callables={}, poststep_callables={}):
 
+        logging.basicConfig(format='\n{}: %(levelname)s: %(message)s\n'.format(
+            self.__class__.__name__), level=LOG_LEVEL)
+        self._logger = logging.getLogger(__name__)
+
+
         self._model = mjp.load_model_from_path(model_path)
         self._sim = mjp.MjSim(self._model)
         self._viewer = mjp.MjViewer(self._sim) if render else None
@@ -63,9 +68,6 @@ class MujocoRobot(object):
         self._mutex = Lock()
         self._asynch_thread_active = False
 
-        logging.basicConfig(format='\n{}: %(levelname)s: %(message)s\n'.format(
-            self.__class__.__name__), level=LOG_LEVEL)
-        self._logger = logging.getLogger(__name__)
 
         self._forwarded = False
 
@@ -83,7 +85,7 @@ class MujocoRobot(object):
 
         if body_name in self._model.site_names:
             self._ee_is_a_site = True
-            self._ee_idx = self._model.site_name2id("ee_site")
+            self._ee_idx = self._model.site_name2id(body_name)
             self._logger.debug(
                 "End-effector is a site in model: {}".format(body_name))
         else:
@@ -165,7 +167,7 @@ class MujocoRobot(object):
         :rtype: np.ndarray (3,), np.ndarray (3,)
         """
         if self._model.sensor_type[0] == 4 and self._model.sensor_type[1] == 5:
-            sensordata = self._sim.data.sensordata.copy()
+            sensordata = -self._sim.data.sensordata.copy() # change sign to make force relative to parent body
             if in_global_frame:
                 if self._ft_site_name:
                     new_sensordata = np.zeros(6)

mujoco_panda/panda_robot.py
@@ -9,7 +9,8 @@ MODEL_PATH = os.environ['MJ_PANDA_PATH'] + \
     '/mujoco_panda/models/franka_panda.xml'
 
 DEFAULT_CONFIG = {
-    'ft_site_name': 'ee_site'
+    'ft_site_name': 'ee_site',
+    'ee_name': 'ee_site'
 }
 
 
@@ -60,10 +61,11 @@ class PandaArm(MujocoRobot):
         self._logger.info("PandaArm: Robot instance initialised with{} gripper".format(
             "out" if not self._has_gripper else ""))
 
-        if self._has_gripper:
-            self.set_as_ee("panda_hand")
-        else:
-            self.set_as_ee("ee_site")
+        if 'ee_name' not in config:
+            if self._has_gripper:
+                self.set_as_ee("panda_hand")
+            else:
+                self.set_as_ee("ee_site")
 
         self._group_actuator_joints()  # identifies position and torque actuators
 
@@ -344,7 +346,7 @@ class PandaArm(MujocoRobot):
 
     def _smoother_handle(self, *args, **kwargs):
         self._smooth_ft_buffer.append(
-            np.append(*(super(self).get_ft_reading(*args, **kwargs))))
+            np.append(*(super(PandaArm, self).get_ft_reading(*args, **kwargs))))
 
     @classmethod
     def fullRobotWithTorqueActuators(cls, **kwargs):