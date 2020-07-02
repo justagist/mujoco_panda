e4d96bffd8d8ca1f632d0ce7ae7b90ecf511f34f
JustaGist <saifksidhik@gmail.com>
2020-07-02 14:54:11 +0100

    add experiment models

--- examples/demo_panda_robot.py
+++ examples/demo_panda_robot.py
@@ -12,21 +12,31 @@ Testing PandaArm instance and parallel command utility
 def exec_func(cmd):
     if cmd == '':
         return None
-    a = eval(cmd)
     print(cmd)
-    print(a)
+    try:
+        if "=" in cmd:
+            exec(cmd)
+            a = "Executed cmd: {}".format(cmd)
+        else:
+            a = eval(cmd)
+            print(a)
+    except Exception as e:
+        a = "{}: {}".format(type(e).__name__, e)
     if a is not None:
         return str(a)
 
+
 if __name__ == "__main__":
     
-    p = PandaArm.fullRobotWithTorqueActuators(render=True,compensate_gravity=True)
+    p = PandaArm.fullRobotWithTorqueActuators(render=False,compensate_gravity=True)
+    p.sim.step()
     p.start_asynchronous_run() # run simulation in separate independent thread
 
-    _cth = ParallelPythonCmd(exec_func)
+    # _cth = ParallelPythonCmd(exec_func)
 
-    while True:
-        p.render()
+    # while True:
+    #     print (p.sim.data.time)
+    #     p.render()
 
 # in the parallel debug interface run PandaArm arm commands to control and monitor the robot.
 # eg: p.set_neutral_pose(), p.joint_angles(), p.ee_pose(), etc.
